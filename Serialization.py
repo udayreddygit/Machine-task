ee9993069a4252fe929132b3701854a30c0d9401
udayreddygit <126895006+udayreddygit@users.noreply.github.com>
2024-12-04 14:41:17 +0530

    Create Serialization.py

---
+++ Serialization.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from .models import Client, Project
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
+
+class ProjectSerializer(serializers.ModelSerializer):
+    users = UserSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Project
+        fields = ['id', 'project_name', 'client', 'users', 'created_at', 'created_by']
+
+class ClientSerializer(serializers.ModelSerializer):
+    projects = ProjectSerializer(many=True, read_only=True)
+    created_by = serializers.StringRelatedField()
+
+    class Meta:
+        model = Client
+        fields = ['id', 'client_name', 'projects', 'created_by', 'created_at', 'updated_at']
